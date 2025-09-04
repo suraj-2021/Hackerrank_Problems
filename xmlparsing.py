4769cef4456ed27dd7ef5e02288f3eddcd947ad9
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2025-09-04 07:41:30 +0530

    Parsing XML in Python
    HACKERRANK Link to the Problem: 
    https://www.hackerrank.com/challenges/xml-1-find-the-score/problem?isFullScreen=true

---
+++ xmlparsing.py
@@ -0,0 +1,16 @@
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(root):
+    score = 0
+    for child in root.iter():
+        score+=len(child.attrib)
+    return score
+    
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
